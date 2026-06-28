34db880569d4ebb8560c848824ed867a85df7a82
Shovon Hasan <im.shovonhasan@gmail.com>
2026-06-28 06:42:35 -0700

    narrow openbao bootstrap root

--- src/infrastructure/bootstrap/openbao-root-bootstrap/main.tf
+++ src/infrastructure/bootstrap/openbao-root-bootstrap/main.tf
@@ -5,18 +5,6 @@ provider "vault" {
 locals {
   kubernetes_auth_mount = "kubernetes"
   kv_mount              = "kv"
-  external_dns_secret   = "guardian/guardian-mgmt/tenant-guardian/dns/external-dns"
-}
-
-resource "vault_mount" "kv" {
-  path        = local.kv_mount
-  type        = "kv-v2"
-  description = "Guardian management cluster secret material."
-
-  options = {
-    version = "2"
-    type    = "kv-v2"
-  }
 }
 
 resource "vault_auth_backend" "kubernetes" {
@@ -29,20 +17,6 @@ resource "vault_kubernetes_auth_backend_config" "guardian_mgmt" {
   backend                = vault_auth_backend.kubernetes.path
   kubernetes_host        = "https://kubernetes.default.svc:443"
   disable_iss_validation = true
-}
-
-resource "vault_policy" "external_dns" {
-  name = "guardian-external-dns"
-
-  policy = <<-EOT
-    path "${local.kv_mount}/data/${local.external_dns_secret}" {
-      capabilities = ["read"]
-    }
-
-    path "${local.kv_mount}/metadata/${local.external_dns_secret}" {
-      capabilities = ["read"]
-    }
-  EOT
 }
 
 resource "vault_policy" "ops_controller" {
@@ -75,17 +49,6 @@ resource "vault_policy" "ops_controller" {
   EOT
 }
 
-resource "vault_kubernetes_auth_backend_role" "external_dns" {
-  backend                          = vault_auth_backend.kubernetes.path
-  role_name                        = "guardian-external-dns"
-  bound_service_account_names      = ["external-dns-secrets"]
-  bound_service_account_namespaces = ["external-dns"]
-  audience                         = "openbao"
-  token_policies                   = [vault_policy.external_dns.name]
-  token_ttl                        = 3600
-  token_max_ttl                    = 3600
-}
-
 resource "vault_kubernetes_auth_backend_role" "ops_controller" {
   backend                          = vault_auth_backend.kubernetes.path
   role_name                        = "guardian-openbao-ops-controller"
@@ -95,4 +58,28 @@ resource "vault_kubernetes_auth_backend_role" "ops_controller" {
   token_policies                   = [vault_policy.ops_controller.name]
   token_ttl                        = 900
   token_max_ttl                    = 3600
+}
+
+removed {
+  from = vault_mount.kv
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = vault_policy.external_dns
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = vault_kubernetes_auth_backend_role.external_dns
+
+  lifecycle {
+    destroy = false
+  }
 }
